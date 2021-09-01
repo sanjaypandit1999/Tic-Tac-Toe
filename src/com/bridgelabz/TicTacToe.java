4e784fe913daa6f9c7807ee405dc2e1798a21b70
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-09-01 12:01:25 +0530

    ADDED:Make move for valid cells

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class TicTacToe {
     public static char playerChoice;
     public static char computerChoice;
+    static char[] board = new char[10];
     public  static char[] CreatBoard(){
-        char[] board = new char[10];
-        for (int i =0; i<board.length; i++){
+        for (int i = 0; i<10; i++){
             board[i] =' ';
         }
         return board;
@@ -26,17 +26,32 @@ public class TicTacToe {
                 System.out.println("INVALID INPUT!!!");
         }
     }
-    public static void displayBoard(char[] BoardCells) {
-        System.out.println("|" + BoardCells[1] + "|" + BoardCells[2] + "|" + BoardCells[3] +"|"
-                + "\n" + "|" + BoardCells[4] + "|" + BoardCells[5] + "|" + BoardCells[6] + "|"
-                + "\n" +"|" + BoardCells[7] + "|" + BoardCells[8] + "|" + BoardCells[9] + "|");
+    public static void displayBoard() {
+        System.out.println("|" + board[1] + "|" + board[2] + "|" + board[3] +"|"
+                + "\n" + "|" + board[4] + "|" + board[5] + "|" + board[6] + "|"
+                + "\n" +"|" + board[7] + "|" + board[8] + "|" + board[9] + "|");
+    }
+     public static boolean isCellAvailable(char[] boardCells, int index) {
+        return boardCells[index] == ' ';
+    }
+    public static void makeMoveForEmptyCells(Scanner scanner) {
+        System.out.println("Player's Turn : ");
+        int movePlayer = scanner.nextInt();
+        if (movePlayer == 0 || movePlayer > 10)
+            System.out.println("INVALID INPUT!!!");
+        else if (isCellAvailable(board, movePlayer)) {
+            board[movePlayer] = playerChoice;
+        } else
+            System.out.println("Position is taken");
     }
 
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char[] initial = CreatBoard();
-        displayBoard(initial);
-
+        chooseLetter(sc);
+        displayBoard();
+        makeMoveForEmptyCells(sc);
+        displayBoard();
     }
 }
